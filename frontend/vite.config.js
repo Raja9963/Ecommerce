03f3d72d2de449e0a0b74199f59523bf5711ef32
Raja143 <thappettarejasekharreddy@gmail.com>
2024-04-04 21:52:34 +0530

    initial commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,8 +6,16 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api/": "http://localhost:5000",
-      "/uploads/": "http://localhost:5000",
+      "/api/":{
+        target: "https://ecommerce-bj60.onrender.com",
+        changeOrigin:true,
+        secure:false,
+        },
+      "/uploads/": {
+        target :"https://ecommerce-bj60.onrender.com",
+      changeOrigin:true,
+    secure:false,
+  }
     },
   },
 });
